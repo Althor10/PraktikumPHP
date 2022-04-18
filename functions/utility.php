1270e9d11890f06e4fef3c8a555ad6048699c7a6
Althor10 <danilozdrale@gmail.com>
2022-04-18 13:07:01 +0200

    Minor changes

--- functions/utility.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function error($errors, $name) {
-    if(isset($errors[$name])) {
-        echo "<div class='alert alert-danger'>" . $errors[$name]  .  "</div>";
-    } else {
-        echo "";
-    }
-}
--- models/functions.php
+++ models/functions.php
@@ -11,3 +11,11 @@ function authorize() {
         redirect("login.php");
     }
 }
+
+function error($errors, $name) {
+    if(isset($errors[$name])) {
+        echo "<div class='alert alert-danger'>" . $errors[$name]  .  "</div>";
+    } else {
+        echo "";
+    }
+}
